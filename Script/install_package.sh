97d40e5883823d12d511940014f7de90cd994610
Aman Bhivgade <bhivgadeaman36@gmail.com>
2026-09-25 15:50:26 +0530

    Create install_package.sh

---
+++ Script/install_package.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+<<info
+
+This script will install the pacakge
+
+that you pass in the arguments
+
+eg. ./install_package.sh nginx
+
+./install_package.sh docker.io
+
+./install_package.sh unzip
+
+info
+
+echo “Installing $1”
+
+sudo apt-get update
+
+sudo apt-get install $1 -y >dev/null
+
+echo “Installation completed” > dev/null
